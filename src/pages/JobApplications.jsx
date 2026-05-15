f97ca2b4ba406759372398ceac3f204da4484972
Abhishek Dave <abhidave2000@gmail.com>
2026-05-14 21:28:04 -0400

    feat(:JobApplication) Add Job Application Static Page

--- src/pages/JobApplications.jsx
+++ src/pages/JobApplications.jsx
@@ -2,13 +2,22 @@ import Navbar from '../components/Navbar';
 
 const JobApplications = () => {
     return(
-        <>
+        <div>
             <Navbar />
-            <div>
-                <h1>Job Applications</h1>
-                <p>Application List Coming here.</p>
+            <div className="max-w-5xl mx-auto px-6 py-8">
+
+                <div className="flex item-center justify-between mb-6">
+                    <h1 className='text-2xl font-bold text-gray-800'>
+                        Job Applications
+                    </h1>
+                    <button className='bg-indigo-800 text-white px-4 py-2 rounded-lg text-sm font-semibold hover:bg-indigo-700 transition'>
+                        + Add Application
+                    </button>
+                </div>
+
+                <p className='text-gray-400'>Applications will show up here.</p>
             </div>
-        </>
+        </div>
     )
 }
 
